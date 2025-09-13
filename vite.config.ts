a303bd9fda4e4b5345c2e60a38fbf0d537f841a7
Basinenimanasa <manasaboya794@gmail.com>
2025-09-13 22:55:57 +0530

    changes in vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/THALLAM-VENKATA-ESWAR-PORTFOLIO/",
   server: {
     host: "::",
     port: 8080,
